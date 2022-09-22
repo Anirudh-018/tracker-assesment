db14d9f7b0c2743a932ee63bd244b2d5abe4870d
Anirudh r k <menonani7@gmail.com>
2022-09-22 22:21:07 +0530

    updated login and page link js

--- trackerjs.js
+++ trackerjs.js
@@ -1,34 +1,54 @@
-function getData() {
-    console.log("clicked");
-    //Get API
-    fetch('https://randomuser.me/api/?results=10')
+fetch('https://randomuser.me/api/?results=10')
         .then(res => res.json())
         .then(data => {
-
-            let author = data.results;
-
-            let output = "<h2><center>Get User Data</center></h2>";
-
-            //Get Data Loop Through
-            const username=[];
-            const password=[];
-            author.forEach(function (lists) {
+            //local storege
+            window.localStorage.setItem('details',JSON.stringify(data.results));
+            items=JSON.parse(localStorage.getItem('details'));
+            console.log(items[0].login.username)
+            console.log(items[0].login.password)
+            console.log(items);
+        });
+function getData() {
+    //Get API
+    items=JSON.parse(localStorage.getItem('details'));
+            var us=document.getElementById("username").value;
+            var pas=document.getElementById("pass").value;
+            var username=[];
+            var password=[]; 
+            items.forEach(function (lists) {
                 username.push(lists.login.username);
                 password.push(lists.login.password);
             });
-            document.getElementById("givenus").innerHTML=username[0];
-            document.getElementById("givenps").innerHTML=password[0];
-            //make these as variables
-            document.getElementById("username").value;
-            document.getElementById("pass").value;
             var n=username.indexOf(us);
-            if(n>0){
+            console.log(n);
+            if(n>=0){
                 if(pas==password[n]){
-                    location.replace("api2.html")
+                    window.open("api2.html")
                 }
                 else{
                     alert("wrong login");
                 }
             }
-        });
-};
+        }
+function display(){
+    items=JSON.parse(localStorage.getItem('details'));
+    let output = "<h2><center>Get User Data</center></h2>";
+    items.forEach(function (lists) {
+        let sal=100000;
+        output += `
+        <div class="container alert alert-success col-sm-3" >
+            <div class="card" style="width:300px">
+                <img class="card-img-top" src="${lists.picture.large}" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">${lists.name.first}</h4>
+                    <h5 class="card-text">${lists.email}</h5>
+                    <h5 class="card-text">${lists.location.country}</h5>
+                    <h5 class="card-text">${sal}</h5>
+                </div>
+            </div>
+        </div> `;
+    });
+    document.getElementById('output').innerHTML = output;
+}
+display()
+
